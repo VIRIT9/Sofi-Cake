8223050431cd633ed449eb9222d033116bb7bbc5
VIRIT9 <virstar9@gmail.com>
2025-05-22 08:16:48 -0300

    Fin Netlify
    Se agrego el código faltante para publicarlo en Netlify

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
     };
   
     // Mantener un intervalo común para todos los carruseles
-    var commonInterval = 3000; // El mismo intervalo usado en la inicialización
+    var commonInterval = 3000; /* El mismo intervalo usado en la inicialización*/
   
     // Función para detener todos los carruseles
     function pauseAllCarousels() {
@@ -44,7 +44,7 @@
   
       carouselElement.addEventListener('mouseleave', function() {
         // Aseguramos que todos los carruseles se reinicien al mismo tiempo
-        setTimeout(resumeAllCarousels, 0); // Usamos setTimeout para asegurar que todos se reinicien en el siguiente tick del evento loop
+        setTimeout(resumeAllCarousels, 0); /* Usamos setTimeout para asegurar que todos se reinicien en el siguiente tick del evento loop*/
       });
     }
 
@@ -63,7 +63,7 @@ const telefonoRegex = /^\d{8,12}$/; // Para teléfonos
 
 
 form.addEventListener('submit', function(event){
-  event.preventDefault(); //Evita que la página se recargue, dando control para validar y enviar los datos.
+  event.preventDefault(); /*Evita que la página se recargue, dando control para validar y enviar los datos.*/
 
   //Resetear errores
 [nombre, email, telefono, mensaje]. forEach(field => {
@@ -71,8 +71,8 @@ form.addEventListener('submit', function(event){
 })
 
  feedback.classList.remove('text-success', 'text-danger', 'text-info');
-    feedback.innerHTML = ''; //Limpia los mensajes anteriores
-/*forEach: Es un método de los arreglos que ejecuta una función para cada elemento de un arreglo [constantes] 
+    feedback.innerHTML = ''; /*Limpia los mensajes anteriores
+/forEach: Es un método de los arreglos que ejecuta una función para cada elemento de un arreglo [constantes] 
 field: es un nombre temporal que representa cada elemento del arreglo en cada iteración. 
 Primera iteración: field es nombre.
 Segunda iteración: field es email.
@@ -101,70 +101,43 @@ classList: es una propiedad de los elementos del DOM,  permite manipular las cla
     mensaje.classList.add('is-invalid');
     isValid = false;
   }
+  /*trim() es un método de los strings que elimina espacios en blanco (espacios, tabuladores, saltos de línea) del inicio y final del texto.
+  test() es un método de las expresiones regulares (RegExp) que verifica si un texto coincide con el patrón de la regex. Devuelve true si coincide, false si no.*/
 
    // Si es válido, enviar con fetch
    if (isValid) {
-    // Crear objeto con los datos
     const submitButton = form.querySelector('button[type="submit"]');
       submitButton.disabled = true;
       feedback.classList.add('text-info');
       feedback.textContent = 'Enviando mensaje...';
-    };
-    //trim() es un método de los strings que elimina espacios en blanco (espacios, tabuladores, saltos de línea) del inicio y final del texto.
-
-    //test() es un método de las expresiones regulares (RegExp) que verifica si un texto coincide con el patrón de la regex. Devuelve true si coincide, false si no.
-
-
-    const formData = new FormData(form);
+      
+      const formData = new FormData(form);
       fetch('/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams(formData).toString()
       })
 
-      /*fetch(URL, opciones)
-      fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST', //enviamos datos al servidor
-        headers: { //especifica que los datos se envian en formato JSON
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData) // Convierte el objeto formData a una cadena JSON con JSON.stringify():
-
-
-      })*/
-
       // Manejar la respuesta
     .then(response => {
-      if (!response.ok) { //response.ok: Verifica si la solicitud fue exitosa (código HTTP 200-299).
-
-
-        throw new Error('Error en la respuesta del servidor'); // throw new Error: Si la respuesta no es válida (por ejemplo, error 404 o 500), lanza un error que se captura en el .catch.
-        }
-      return response.json(); // Convierte la respuesta del servidor (que viene como texto JSON) en un objeto JS
-      
-    })
-
-    // Éxito
-    .then(data => { // Contiene la respuesta del servidor
-
+      if (!response.ok) throw new Error('Error en la respuesta del servidor'); /* throw new Error: Si la respuesta no es válida (por ejemplo, error 404 o 500), lanza un error que se captura en el .catch. { //response.ok: Verifica si la solicitud fue exitosa (código HTTP 200-299).*/
       feedback.classList.remove('text-info');
       feedback.classList.add('text-success');
       feedback.textContent = '¡Mensaje enviado con éxito!'; //feedback: Cambia el mensaje a "¡Mensaje enviado con éxito!" y usa la clase text-success (verde) de Bootstrap.
+      submitButton.disabled = false;
       form.reset(); // Limpia el formulario
-      console.log('Respuesta del servidor:', data);
+      console.log('Formulario envidado a Netlify');
     })
-
-    //Manejar errores
-    .catch(error => { //Captura cualquier error (problema de red, servidor no disponible, etc.).
     
+    //Manejar errores
+    .catch(error => { /*Captura cualquier error (problema de red, servidor no disponible, etc.).*/
       feedback.classList.remove('text-info');
       feedback.classList.add('text-danger');
       feedback.textContent = 'Error al enviar el mensaje. Intente de nuevo.';
+      submitButton.disabled = false;
       console.error('Error:', error);
     });
-} else {
-  // Mostrar errores
-  feedback.classList.remove('text-success');
+} else { // Mostrar errores
   feedback.classList.add('text-danger');
   feedback.textContent = 'Por favor, corrija los errores.';
 }
