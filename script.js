6290eb0541687f7bd0eb45f5e85c157ecfbfc085
VIRIT9 <virstar9@gmail.com>
2025-05-24 23:22:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -114,9 +114,11 @@ classList: es una propiedad de los elementos del DOM,  permite manipular las cla
       const formData = new FormData(form);
       fetch('/', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(formData).toString()
-      })
+        body: new URLSearchParams({
+          'form-name': 'contactForm',
+          ...Object.fromEntries(formData)
+  }).toString()
+})
 
       // Manejar la respuesta
     .then(response => {
